de18f14398bd84d4bc1b36241bfaca0728f45afe
debbie chen <debbie.cc.chen@gmail.com>
2018-03-13 11:22:50 +0800

    fix scope

--- www/js/create-user.js
+++ www/js/create-user.js
@@ -1,18 +1,18 @@
 app.controller('CreateUserCtrl', function ($scope, $rootScope, $state, $http, $ionicPopup, $ionicHistory, $firebaseAuth) {
 
-    $scope.name = 'test';
-    $scope.description = 'test';
-
+    
+    $scope.profile = {
+      name: '',
+      description: ''
+    };
 
 
     $scope.submit = function () {
       var userId = $state.params.userId;
-      var body = {
-        name: $scope.name,
-        description: $scope.description
-      };
-      firebase.database().ref('users/' + userId).set(body).then(function (result) {
-        alert('Success !');
+      
+      firebase.database().ref('users/' + userId).set($scope.profile).then(function (result) {
+        alert('success');
+        $state.go('main-tabs.post-list');
       }, function (err) {
         console.error(err);
       });
