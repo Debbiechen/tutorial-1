638da6581c19411ab7bcd3d2cea48879eff4d9c0
cookyproject <cookyproject@gmail.com>
2018-03-22 01:57:57 +0800

    fix add item ui

--- www/js/create-post.js
+++ www/js/create-post.js
@@ -1,25 +1,31 @@
-app.controller('CreatePostCtrl', function ($scope, $rootScope, $state, $http, $ionicPopup, $ionicHistory,$firebaseObject, $firebaseAuth,me) {
-    
-    $scope.me = me;
-    $scope.post = {
-        content: ''
-    };
-    $scope.items = [{
+app.controller('CreatePostCtrl', function ($scope, $rootScope, $state, $http, $ionicPopup, $ionicHistory, $firebaseObject, $firebaseAuth, me, Guid) {
+
+
+  $scope.me = me;
+  $scope.content = '';
+  $scope.items = [{
+    name: ''
+  }];
+
+  $scope.onItemNameChanged = function (index) {
+    if ($scope.items[index].name.length == 0 && index < $scope.items.length) {
+      $scope.deleteItem(index);
+    } else if (index == $scope.items.length - 1) {
+      $scope.items.push({
         name: ''
-    }];
-    
-    $scope.onItemNameChanged = function(index){
+      });
+    }
 
-    };
-    $scope.canDeleteItem = function(index){
-        return index < $scope.items.length-1;
-    };
-    $scope.deleteItem = function(index){
+  };
+  $scope.canDeleteItem = function (index) {
+    return index < $scope.items.length - 1 && $scope.items[index].name.length > 0;
+  };
+  $scope.deleteItem = function (index) {
+    $scope.items.splice(index, 1);
+  };
+  $scope.init = function () {
 
-    };
-    $scope.init = function(){
-        
-    };
-    $scope.init();
+  };
+  $scope.init();
 
 });
